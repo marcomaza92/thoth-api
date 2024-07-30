56c57fb4f833258ea64dff1740957a282de8b331
Francisco Gil <francisconicolasgil@gmail.com>
2024-07-29 21:27:40 -0300

    feat: Add nginx conf file

--- database/init.sql
+++ database/init.sql
@@ -1,12 +1,39 @@
 -- Crear la base de datos (si no existe)
-CREATE DATABASE IF NOT EXISTS mydatabase;
 
--- Conectarse a la base de datos
-\c mydatabase;
+DO $$
+BEGIN
+    -- Verifica si la base de datos existe
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mydatabase') THEN
+        -- Crea la base de datos
+        EXECUTE 'CREATE DATABASE mydatabase';
+        
+        -- Mensaje en la consola
+        RAISE NOTICE 'mydatabase created by Init script';
+    ELSE
+        RAISE NOTICE 'mydatabase already existed';
+    END IF;
+END
+$$;
 
--- Crear la tabla de usuarios (si no existe)
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    age INTEGER NOT NULL
-);
+-- Conecta a la base de datos
+\c mydatabase
+
+DO $$
+BEGIN
+    -- Verifica si la tabla "users" existe
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
+        -- Crea la tabla "users"
+        EXECUTE '
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            age INTEGER NOT NULL
+        )';
+        
+        -- Mensaje en la consola
+        RAISE NOTICE 'users table created by Init script';
+    ELSE
+        RAISE NOTICE 'users table already existed';
+    END IF;
+END
+$$;
